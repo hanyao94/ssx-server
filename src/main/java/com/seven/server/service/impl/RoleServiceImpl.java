e5723c2adc99c71dfa1df13b402ae250773d45c2
hanyao94 <hanyao.huang@foxmail.com>
2018-07-15 16:11:57 +0800

    RolePermissionMapper添加保存角色权限方法，角色接口实现，重写save,update方法，在保存角色的时候，将对应权限，保存进去

--- src/main/java/com/seven/server/service/impl/RoleServiceImpl.java
+++ src/main/java/com/seven/server/service/impl/RoleServiceImpl.java
@@ -1,11 +1,15 @@
 package com.seven.server.service.impl;
 
 import com.seven.server.mapper.RoleMapper;
+import com.seven.server.mapper.RolePermissionMapper;
 import com.seven.server.model.Role;
+import com.seven.server.model.RolePermission;
 import com.seven.server.service.RoleService;
 import com.seven.server.core.service.AbstractService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
+import tk.mybatis.mapper.entity.Condition;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -18,11 +22,29 @@ import java.util.List;
 @Transactional
 @SuppressWarnings("SpringJavaAutowiringInspection")
 public class RoleServiceImpl extends AbstractService<Role> implements RoleService {
-    @Resource
-    private RoleMapper roleMapper;
+  @Resource
+  private RoleMapper roleMapper;
 
-    @Override
-    public List<com.seven.server.model.Resource> findAllRoleWithPermission() {
-        return roleMapper.findAllRoleWithPermission();
-    }
+  @Resource
+  private RolePermissionMapper rolePermissionMapper;
+
+  @Override
+  public List<com.seven.server.model.Resource> findAllRoleWithPermission() {
+    return roleMapper.findAllRoleWithPermission();
+  }
+
+  @Override
+  public void save(Role role) {
+    roleMapper.insert(role);
+    this.rolePermissionMapper.saveRolePermission(role.getId(), role.getPermissionIdList());
+  }
+
+  @Override
+  public void update(Role role) {
+    // 删掉所有权限，再添加回去
+    final Condition condition = new Condition(RolePermission.class);
+    condition.createCriteria().andCondition("role_id = ", role.getId());
+    rolePermissionMapper.deleteByCondition(condition);
+    rolePermissionMapper.saveRolePermission(role.getId(), role.getPermissionIdList());
+  }
 }
